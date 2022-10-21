fdf150ff17603307f80f076616384bfe7c2d64ff
Yana Evstegneeva <yanka.evste@gmail.com>
2022-10-21 14:25:16 +0500

    fix last commit #3

--- controllers/users.js
+++ controllers/users.js
@@ -84,7 +84,7 @@ module.exports.login = (req, res, next) => {
         { _id: user._id },
         secretKey,
         { expiresIn: '7d' });
-      console.log('qweqwe = ', secretKey);
+      console.log('login qweqwe = ', secretKey);
       res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true
