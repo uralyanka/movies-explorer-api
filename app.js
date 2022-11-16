39fe8ad26ac2cf7eebd6eb5943cd7a716dbd66ae
Yana Evstegneeva <yanka.evste@gmail.com>
2022-11-16 18:44:41 +0500

    upd CORS

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
-  origin: 'https://uralyanka.mesto.nomoredomains.icu',
+  origin: 'https://uralyanka.diploma.nomoredomains.icu',
   credentials: true,
 }));
 app.use(cors);
