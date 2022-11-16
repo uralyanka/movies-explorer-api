a6f563fb99d307f9af3d3c0945ab67f24b766d3e
Yana Evstegneeva <yanka.evste@gmail.com>
2022-11-16 18:30:50 +0500

    add CORS

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const NotFoundError = require('./errors/notFoundError');
 const auth = require('./middlewares/auth');
 const errorsHandler = require('./middlewares/errorsHandler');
@@ -19,6 +20,11 @@ const app = express();
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({
+  origin: 'https://uralyanka.mesto.nomoredomains.icu',
+  credentials: true,
+}));
+app.use(cors);
 
 mongoose.connect(mongodb);
 
